2d611e3239ed371912ff7ace21883780a8b16475
dlxks <dlxks.sangangbayan@gmail.com>
2025-07-28 20:40:34 +0800

    Initial commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,80 @@
+import { navLinks } from "@/data/navLinks";
+import Image from "next/image";
+import { Icon } from "@iconify/react";
+
+const Footer = () => {
+  return (
+    <footer className="text-charcoal-500">
+      <div className="container mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 bg-mist-500 py-8">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-8">
+          <div className="grid grid-cols-2">
+            <div>
+              <Image
+                src="/assets/Asset 20@4x.png"
+                alt="Little Lemon Logo"
+                width={75}
+                height={30}
+              />
+            </div>
+            <div>
+              <ul>
+                {navLinks.map((item) => (
+                  <li
+                    key={item.name}
+                    className="text-md text-olive-500 hover:text-olive-700 mb-2"
+                  >
+                    <a href={item.href}>{item.name}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div>
+            <h5 className="font-black text-xl mb-4">Contact Us!</h5>
+            <ul className="text-olive-400 text-md">
+              <li className="flex items-center space-x-2">
+                <Icon icon="bi:telephone-fill" />
+                &nbsp;<span>(+1) 123-456-7890</span>
+              </li>
+              <li className="flex items-center space-x-2">
+                <Icon icon="bi:envelope-at-fill" />
+                &nbsp;<span>littlelemon@example.com</span>
+              </li>
+              <li className="flex items-center space-x-2">
+                <Icon icon="bi:link-45deg" />
+                &nbsp;<span>littlelemon.com</span>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h5 className="font-black text-xl mb-4">
+              Find Us On Social Media!
+            </h5>
+            <ul className="flex text-2xl md:text-4xl text-olive-400 gap-4 transition-all duration-150 ease-in-and-out">
+              <li>
+                <Icon icon="bi:facebook" />
+              </li>
+              <li>
+                <Icon icon="bi:instagram" />
+              </li>
+              <li>
+                <Icon icon="bi:twitter-x" />
+              </li>
+              <li>
+                <Icon icon="bi:youtube" />
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="w-full text-md text-center text-charcoal-500 mt-4">
+          <div className="border border-charcoal-300"></div>
+          <p className="mt-4 text-olive-700">
+            Little Lemon &copy;{new Date().getFullYear()}{" "}
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/data/navLinks.ts
@@ -0,0 +1,7 @@
+export const navLinks = [
+  { name: "Home", href: "./", current: true },
+  { name: "About", href: "#", current: false },
+  { name: "Menu", href: "#", current: false },
+  { name: "Reservations", href: "#", current: false },
+  { name: "Order Online", href: "#", current: false },
+];
